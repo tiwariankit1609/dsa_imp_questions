d0527733984a899bb4a6f839027fb48ed3b7c28b
Ankit Tiwari <tiwariankit1609@gmail.com>
2026-05-29 11:54:33 +0530

    added linked list file

---
+++ OneDrive/Desktop/Tiwari2/Linkedlist/ImplementationLl.java
@@ -0,0 +1,36 @@
+package Linkedlist;
+
+public class ImplementationLl {
+    public static void Node(int val){
+        int data;
+        Node next;
+       void Node(val){
+            this.data;
+            this.Node;
+        }
+    }
+     int getAt(int idx){
+        if(idx<0||idx>size()){
+            System.out.println("wrong index");
+        Node temp = head;
+        for(it i=0;i<=idx;i++){
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
+        return temp.data;
+    void display(){
+        Node temp = head;
+        while(temp!=null){
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+        
+        
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
